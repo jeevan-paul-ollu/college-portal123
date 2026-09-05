db842e48e2a058b9f4ef6011f6a2fda1e8d4944f
Ollu Jeevan Paul <jeevanpaulg49@gmail.com>
2026-09-05 18:42:09 +0530

    modified 1

--- college_portal/settings.py
+++ college_portal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY","temporary-development-key")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("Debug","False")=="True"
+DEBUG = os.environ.get("DEBUG","False")=="True"
 
 ALLOWED_HOSTS = [
     ".vercel.app",
